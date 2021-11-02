abf2604114edf1e3301baa33c60a94ec8e7eb7de
Bala Manikanta Pulleti <pulleti.bala@cleanharbors.com>
2021-11-02 15:05:49 +0530

    logging

---
+++ LoggingLibrary/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using System.Diagnostics;
+
+
+namespace LoggingLibrary
+{
+  public class Logger : ILogger
+  {
+    public void LogMessage(string componentName, string action, TimeSpan timeSpan, string message)
+    {
+      string str = $"-----------------------------------------------------" + $"\n\tComponentName:{componentName},Action:{action},TimeSpan:{timeSpan} and Message:{message}\n" + "---------------------------";
+      Debug.WriteLine(str);
+    }
+  }
+}
